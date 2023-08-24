2487a1d72ecfa3b638db15be4347512fd76efcee
Harold Sun <sunhua@amazon.com>
2023-08-24 06:31:54 +0000

    Setup container insights

--- data_plane/backend_workers/infrastructure.ts
+++ data_plane/backend_workers/infrastructure.ts
@@ -6,6 +6,7 @@ import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as logs from 'aws-cdk-lib/aws-logs';
 
 export class EksClusterConstruct extends Construct {
     outputBucket: s3.IBucket;
@@ -21,6 +22,17 @@ export class EksClusterConstruct extends Construct {
             irsaRoles: ["CloudWatchFullAccess", "AmazonSQSFullAccess"]
         }
 
+        const CloudWatchLogsWritePolicy = new iam.PolicyStatement({
+            actions: [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents"
+            ],
+            resources: ["*"],
+        })
+
         const addOns: Array<blueprints.ClusterAddOn> = [
             new blueprints.addons.VpcCniAddOn(),
             new blueprints.addons.CoreDnsAddOn(),
@@ -30,9 +42,21 @@ export class EksClusterConstruct extends Construct {
             new blueprints.addons.AwsLoadBalancerControllerAddOn(),
             new blueprints.addons.KarpenterAddOn({ interruptionHandling: true }),
             new blueprints.addons.KedaAddOn(kedaParams),
-            new blueprints.addons.CertManagerAddOn(),
-            new blueprints.addons.AdotCollectorAddOn(),
-            new blueprints.addons.XrayAdotAddOn(),
+            new blueprints.addons.ContainerInsightsAddOn(),
+            new blueprints.addons.AwsForFluentBitAddOn({
+                iamPolicies: [CloudWatchLogsWritePolicy],
+                namespace: "amazon-cloudwatch",
+                values: {
+                    cloudWatchLogs: {
+                        region: "us-west-2",
+                    },
+                    tolerations: [{
+                        "key": "nvidia.com/gpu",
+                        "operator": "Exists",
+                        "effect": "NoSchedule"
+                    }]
+                }
+            }),
         ];
 
         const blueprint = blueprints.EksBlueprint.builder()
@@ -42,24 +66,41 @@ export class EksClusterConstruct extends Construct {
 
         const cluster = blueprint.getClusterInfo().cluster
 
-        const WebUISA = cluster.addServiceAccount('WebUISA', {
-            name: 'sd-webui-sa',
+        const xrayDaemonSA = cluster.addServiceAccount('XrayDaemonSA', {
+            name: 'xray-daemon',
+            namespace: 'amazon-cloudwatch'
+        })
+
+        xrayDaemonSA.addToPrincipalPolicy(new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            resources: ["*"],
+            actions: [
+                "xray:PutTraceSegments",
+                "xray:PutTelemetryRecords",
+                "xray:GetSamplingRules",
+                "xray:GetSamplingTargets",
+                "xray:GetSamplingStatisticSummaries"
+            ]
+          }))
+
+        const webUISA = cluster.addServiceAccount('WebUISA', {
+            name: 'sd-webui-sa'
         })
 
         const modelBucket = s3.Bucket.fromBucketAttributes(this, 'ModelBucket', {
             bucketArn: 'arn:aws:s3:::sd-on-eks-pdx',
         });
-        modelBucket.grantRead(WebUISA);
+        modelBucket.grantRead(webUISA);
 
         const inputQueue1 = new sqs.Queue(this, 'InputQueue1');
-        inputQueue1.grantConsumeMessages(WebUISA);
+        inputQueue1.grantConsumeMessages(webUISA);
 
         this.outputBucket = new s3.Bucket(this, 'sd-output-pdx');
-        this.outputBucket.grantWrite(WebUISA);
-        this.outputBucket.grantPutAcl(WebUISA);
+        this.outputBucket.grantWrite(webUISA);
+        this.outputBucket.grantPutAcl(webUISA);
 
         const notificationTopic = new sns.Topic(this, 'sd-notification-pdx');
-        notificationTopic.grantPublish(WebUISA);
+        notificationTopic.grantPublish(webUISA);
 
     }
 }
