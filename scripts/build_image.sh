f20c0e614297a400d6898472531416ee96dbee43
bingjiao <bingjiao@amazon.com>
2023-08-30 23:51:11 +0800

    Implicit set working directory

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/bin/bash
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 IMAGE_TAG=$(git rev-parse --short HEAD)
 REPO=public.ecr.aws/bingjiao
@@ -11,11 +12,11 @@ aws ecr-public get-login-password --region us-east-1 | helm registry login publi
 # aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 # Build images
-docker build -t $AGENT_IMAGE_NAME:$IMAGE_TAG src/queue_agent/
+docker build -t $AGENT_IMAGE_NAME:$IMAGE_TAG ../src/queue_agent/
 docker tag $AGENT_IMAGE_NAME:$IMAGE_TAG $REPO/$AGENT_IMAGE_NAME:$IMAGE_TAG
 docker push $REPO/$AGENT_IMAGE_NAME:$IMAGE_TAG
 
-docker build -t $SDWEBUI_IMAGE_NAME:$IMAGE_TAG src/sd_webui_api/
+docker build -t $SDWEBUI_IMAGE_NAME:$IMAGE_TAG ../src/sd_webui_api/
 docker tag $SDWEBUI_IMAGE_NAME:$IMAGE_TAG $REPO/$SDWEBUI_IMAGE_NAME:$IMAGE_TAG
 docker push $REPO/$SDWEBUI_IMAGE_NAME:$IMAGE_TAG
 
@@ -25,4 +26,5 @@ echo "$REPO/$SDWEBUI_IMAGE_NAME:$IMAGE_TAG"
 
 # Build EBS snapshot
 echo "Building EBS snapshot: "
-/bin/bash -c $(echo "utils/snapshot.sh $REPO/$AGENT_IMAGE_NAME:$IMAGE_TAG, $REPO/$SDWEBUI_IMAGE_NAME:$IMAGE_TAG")
+cd ../utils/bottlerocket-images-cache/
+./snapshot.sh $REPO/$AGENT_IMAGE_NAME:$IMAGE_TAG, $REPO/$SDWEBUI_IMAGE_NAME:$IMAGE_TAG
